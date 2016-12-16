eba0dc5e1f65d141f782154cfa3b35ba8e841509
NGUYEN Manh Linh <NGUYEN Manh Linh>
2016-12-16 16:34:10 +0700

    add font and update css

--- app/js/ongvang.js
+++ app/js/ongvang.js
@@ -1,5 +1,10 @@
 var personal = 'Cá Nhân',
     company = 'Doanh Nghiệp';
+// check email validation
+var validateEmail = function(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $('a.page-scroll').bind('click', function(event) {
@@ -28,14 +33,6 @@ $('#mainNav').affix({
     }
 });
 
-$('.forgot-pass').click(function(event) {
-    $(".pr-wrap").toggleClass("show-pass-reset");
-}); 
-
-$('.pass-reset-submit').click(function(event) {
-    $(".pr-wrap").removeClass("show-pass-reset");
-});
-
 $('.input-submit').click(function() {
     var valueSelected = $('#sel1 option:selected').text();
     if (valueSelected != personal && valueSelected != company) {
@@ -56,6 +53,26 @@ $('.input-submit').click(function() {
     }
 })
 
+$('.request-submit').click(function(e) {
+    var currentVal = $('.email-request').val();
+    if (!currentVal.length || !validateEmail(currentVal)) {
+        $('.alert-success-show').css('display', 'none');
+        $('.alert-fail-show').css('display', 'block');
+        setTimeout(function() {
+            $('.alert-fail-show').css('display', 'none');
+        }, 3000)
+    } else {
+        $('.email-request').val('');
+        $('.alert-fail-show').css('display', 'none');
+        $('.alert-success-show').css('display', 'block');
+        setTimeout(function() {
+            $('.alert-success-show').css('display', 'none');
+        }, 3000)
+    }
+    
+    return;
+})
+
 $('.btn-sign-up-edit').click(function () {
     $('.form-bg').css('display', 'none');
     $('.form-msg-done').css('display', 'block');
@@ -67,7 +84,6 @@ $('.btn-sign-up-edit').click(function () {
         $('.personal-msg').css('display', 'none');
         $('.company-msg').css('display', 'block');
     }
-
 })
 
 function myMap() {
